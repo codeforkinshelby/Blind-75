09d0e7bc5d3a66c4f89bf2faeba5f231790b7bea
Eashaan M <110089354+eashaanak7522@users.noreply.github.com>
2023-07-10 12:51:16 +0530

    Add files via upload

---
+++ buysellstock.py
@@ -0,0 +1,21 @@
+#You are given an array prices
+#where prices[i] is the price of a given stock on the ith day.
+#You want to maximize your profit
+#by choosing a single day to buy one stock
+#and choosing a different day in the future to sell that stock.
+#Return the maximum profit you can achieve from this transaction.
+#If you cannot achieve any profit, return 0.
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        buy = prices[0]
+        for price in prices[1:]:
+            if price<buy:
+                buy = price
+            elif price-buy> maxProfit:
+                maxProfit = price-buy
+        return maxProfit
